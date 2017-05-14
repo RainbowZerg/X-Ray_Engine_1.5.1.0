801aac9becbe4d88a6f83f149f8764ec6de75220
RainbowZerg <zloyzergo@gmail.com>
2017-05-14 22:18:55 +0300

    CSoundRender_Source::load - no crash if sound not found

--- src/xrSound/SoundRender_Source_loader.cpp
+++ src/xrSound/SoundRender_Source_loader.cpp
@@ -136,13 +136,15 @@ void CSoundRender_Source::load(LPCSTR name)
 	fname				= N;
 
 	strconcat			(sizeof(fn),fn,N,".ogg");
-	if (!FS.exist("$level$",fn))	FS.update_path	(fn,"$game_sounds$",fn);
+	if (!FS.exist("$level$",fn))	
+		FS.update_path	(fn,"$game_sounds$",fn);
 
-#ifdef _EDITOR
-	if (!FS.exist(fn)){ 
+	if (!FS.exist(fn))
+	{ 
+		Msg("! Can't find sound '%s.ogg'", N);
 		FS.update_path	(fn,"$game_sounds$","$no_sound.ogg");
     }
-#endif
+
 	LoadWave			(fn);
 	SoundRender->cache.cat_create	(CAT, dwBytesTotal);
 }
